dfac1b0770e658eb2e2927008942ab21764e6363
movieshome <moviesahome@gmail.com>
2025-02-25 23:21:56 +0530

    Update screenechobot.py

--- screenechobot.py
+++ screenechobot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 
 # Start command handler
 def start(update: Update, context: CallbackContext):
